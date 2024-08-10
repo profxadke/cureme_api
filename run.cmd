b77a322032a2c807eee0a3a78fe2f4de0846582c
Prof. - Xadk3! <xadkeg@duck.com>
2024-08-11 03:22:38 +0545

    fixed WiNd0eZ issue but one still presists and says, The system cannot find the file specified. :| Also, why on earth I need to be specific 'n' to Terminate batch job (Y/N)? T-T fml

--- run.cmd
+++ run.cmd
@@ -3,7 +3,8 @@ title CureMeAPI
 
 echo [!] Make sure, you've already installed all the required dependencies, along with environment setup and stuff.
 echo [!] ALSO DO MAKE SURE YOU'VE CONFIGURED YOUR .env FILE.
+echo.
+echo [+] Respond with n/N while it asks to weather to terminate batch job or not, since we need to perform a bit of cleanup.
+echo.
 uvicorn app.app:api --host 0.0.0.0 --port 8888
-rmdir /s /q __pycache__ app/__pycache__ app/crud/__pycache__ app/models/__pycache__ app/routers/__pycache__ app/schemas/__pycache__
-echo 
-echo NOTE: even though you are no to upcoming prompt, its still gonna quit.
+rmdir /s /q "__pycache__" "app/__pycache__" "app/crud/__pycache__" "app/models/__pycache__" "app/routers/__pycache__" "app/schemas/__pycache__"
